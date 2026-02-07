644f94269a63e0ecaa9d9cf966e9eb344b6956c4
Lerrell Bentley <loonjunior@gmail.com>
2026-02-07 04:06:06 -0600

    first commit

---
+++ utils/physics.h
@@ -0,0 +1,10 @@
+#pragma once
+
+constexpr float gravity = 640;
+constexpr float damping = 0.9;
+constexpr float walk_acceleration = 700;
+constexpr float jump_velocity = 320;
+constexpr float terminal_velocity = 1000;
+
+class physics {
+};
